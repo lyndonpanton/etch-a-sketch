5912ceb9ba9414f6f02fd8515db22d4aea0e9b01
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-22 15:05:04 +0100

    Feature: Alpha component of each colour is randomised

--- js/script.js
+++ js/script.js
@@ -12,20 +12,56 @@ function createGrid(length) {
         square.classList.add("square");
         square.style.width = (containerLength / length) + "px";
         square.style.height = (containerLength / length) + "px";
+        square.style.background = "rgba(255, 255, 255, 1.0)";
         square.addEventListener("mouseenter", enterSquare);
-        square.addEventListener("mouseleave", enterSquare);
+        square.addEventListener("mouseleave", leaveSquare);
 
         container.appendChild(square);
     }
 }
 
 function enterSquare(e) {
+    let background = e.target.style.background;
+
     const redValue = Math.floor(Math.random() * 256);
     const greenValue = Math.floor(Math.random() * 256);
     const blueValue = Math.floor(Math.random() * 256);
-    const colour = "rgba(" + redValue + ", " + greenValue + ", " + blueValue + ", 1)";
 
-    e.target.style.background = colour;
+    if (background.indexOf("a") === -1) {
+        background =
+                "rgba(" + redValue + ", " + greenValue + ", " + blueValue
+                + ", 0.1)";
+    } else {
+
+        let lastComma = e.target.style.background.lastIndexOf(",");
+        let closingBracket = e.target.style.background.indexOf(")");
+
+        let alpha = parseFloat(e.target.style.background.slice(lastComma + 2, closingBracket));
+
+        let newAlpha;
+
+        switch (alpha) {
+            case 0.1:
+            case 0.2:
+            case 0.3:
+            case 0.4:
+            case 0.5:
+            case 0.6:
+            case 0.7:
+            case 0.8:
+            case 0.9:
+                newAlpha = alpha + 0.1;
+                break;
+        }
+
+        background =
+                "rgba(" + redValue + ", " + greenValue + ", " + blueValue
+                + ", " + newAlpha + ")";
+
+        background = background.replace(alpha, newAlpha);
+    }
+
+    e.target.style.background = background;
 }
 
 function leaveSquare(e) {
