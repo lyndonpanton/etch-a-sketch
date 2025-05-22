009faf9d924a49519472c700a9fbbfad2969f3e7
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-22 13:56:36 +0100

    Feature: User can choose the dimension of the grid

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+function createGrid(length) {
+    let container = document.getElementById("container");
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    let containerLength = container.offsetWidth;
+    
+    for (let i = 0; i < (length * length); i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = (containerLength / length) + "px";
+        square.style.height = (containerLength / length) + "px";
+        square.addEventListener("mouseenter", enterSquare);
+        square.addEventListener("mouseleave", enterSquare);
+
+        container.appendChild(square);
+    }
+}
+
 function enterSquare(e) {
     if (!e.target.classList.contains("square-filled")) {
         e.target.classList.add("square-filled");
@@ -8,15 +29,28 @@ function leaveSquare(e) {
 
 }
 
-document.addEventListener("DOMContentLoaded", function(e) {
-    let container = document.getElementById("container");
-    
-    for (let i = 0; i < (16 * 16); i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseenter", enterSquare);
-        square.addEventListener("mouseleave", enterSquare);
+function askForNewLength(e) {
+    let newLength;
 
-        container.appendChild(square);
+    while (true) {
+        newLength = prompt("How many squares per sides would you like?");
+
+        if (newLength >= 16 && newLength <= 100) {
+            break;
+        } else {
+            alert(
+                "Error: Squares per size must be between 16 (inclusive) and 100 "
+                + "(inclusive)"
+            );
+        }
     }
+
+    createGrid(newLength);
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    createGrid(16);
+
+    let resizeButton = document.getElementById("resize");
+    resizeButton.addEventListener("click", askForNewLength);
 });
