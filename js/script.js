967a40c26557379f0b7a711cc62d2fd1938db8b7
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-22 13:28:51 +0100

    Feature: Squares change colour when hovered over

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
+function enterSquare(e) {
+    if (!e.target.classList.contains("square-filled")) {
+        e.target.classList.add("square-filled");
+    }
+}
+
+function leaveSquare(e) {
+
+}
+
 document.addEventListener("DOMContentLoaded", function(e) {
-    console.log("ok");
     let container = document.getElementById("container");
     
     for (let i = 0; i < (16 * 16); i++) {
         let square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseenter", enterSquare);
+        square.addEventListener("mouseleave", enterSquare);
 
         container.appendChild(square);
     }
