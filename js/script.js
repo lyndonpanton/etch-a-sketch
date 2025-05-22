3a9b06aec73e85a0d8dd9a37129323528b640547
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-22 13:20:17 +0100

    Feature: Add grid of squares to container

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("ok");
+    let container = document.getElementById("container");
+    
+    for (let i = 0; i < (16 * 16); i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+
+        container.appendChild(square);
+    }
+});
